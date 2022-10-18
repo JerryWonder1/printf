d6c4f311461070c9a71c3cc8bfb35c02c5d25513
Joseph Jeremiah <jerrycul2001@yahoo.com>
2022-10-18 21:04:36 +0000

    First commit

---
+++ _printf.c
@@ -0,0 +1,9 @@
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, format);
+
+	
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef FUNC
+#define FUNC
+
+print_char(char *s);
+print_string(char *s);
+
+
+#endif
